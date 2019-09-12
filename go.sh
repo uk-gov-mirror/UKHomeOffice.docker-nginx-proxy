28151a8615b1bdf374dded17740417390ff10dc9
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2019-09-12 10:34:05 +0100

    Correct NGINX_REAL_IP_CONF var name

--- go.sh
+++ go.sh
@@ -45,7 +45,7 @@ EOF-LISTEN
 if test -n "${REAL_IP_HEADER:-}" -a -n "${REAL_IP_FROM:-}"; then
     NGIX_REAL_IP_CONF="${NGINX_CONF_DIR}/nginx_server_extras_real_ip.conf"
     touch ${NGINX_REAL_IP_CONF}
-cat > ${NGIX_REAL_IP_CONF} <<-EOF-REALIP
+cat > ${NGINX_REAL_IP_CONF} <<-EOF-REALIP
     set_real_ip_from '${REAL_IP_FROM}';
     real_ip_header '${REAL_IP_HEADER}';
     real_ip_recursive on;
